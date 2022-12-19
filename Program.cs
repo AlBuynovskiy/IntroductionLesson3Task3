9da04f116c2496e70a0d529163f7a32a8be0ac79
AlBuynovskiy <AlBuynovskiy@gmail.com>
2022-12-19 19:13:04 +0300

    Added task condition

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N. int[] array =
+*/
