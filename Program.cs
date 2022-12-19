8edd6270c83a5848accbe6b71ade404d687e5980
AlBuynovskiy <AlBuynovskiy@gmail.com>
2022-12-19 19:25:29 +0300

    Debugging program 1

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
 Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N. int[] array =
 */
 
-
 Console.Write("Введите кол-во чисел для вывода: ");
 int N = int.Parse(Console.ReadLine());
  
@@ -12,9 +11,3 @@ for(int i = 1; i <= N; i++)
 
 Console.WriteLine(i + "\t" + i*i);
 Console.ReadLine();
-
-  
-   
-
-
-  
